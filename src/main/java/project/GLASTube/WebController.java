cb4a690a34be2f3070c2c03648d730a5f2bcee4b
AlexeyQWE <mountain.skier@yadnex.ru>
2019-08-21 13:32:41 +0700

    Add autorisation and connect to home page

--- src/main/java/project/GLASTube/WebController.java
+++ src/main/java/project/GLASTube/WebController.java
@@ -1,11 +1,16 @@
 package project.GLASTube;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
+
+    @Autowired
+    private UserRepository crudRep;
 
     @RequestMapping("/")
     public String indexStart(Model model) {
@@ -18,5 +23,21 @@ public class WebController {
         model.addAttribute("name", "Sign Up");
 
         return "indexSignUp";
+    }
+    @RequestMapping("/sign_in")
+    public String indexSignIn(Model model) {
+        model.addAttribute("name", "Sign In");
+
+        return "indexSignIn";
+    }
+    @RequestMapping("/sign_in/{login}/{password}")
+    public String indexMainPage(@PathVariable("login") String login, @PathVariable("password") String password, Model model) {
+        model.addAttribute("name", "Home Page");
+        User user = crudRep.findByLogin(login);
+
+        if (user.getLogin().equals(login) && user.getPassword().equals(password))
+            return  "indexMainPage";
+
+        return null;
     }
 }
