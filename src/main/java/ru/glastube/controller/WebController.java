c3b761eb5af682b61c6904be78db75b97dc02876
Stalker2973 <omelete@mail.ru>
2019-08-22 16:55:04 +0700

    Comments

--- src/main/java/ru/glastube/controller/WebController.java
+++ src/main/java/ru/glastube/controller/WebController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.glas***.entity.Comments;
 import ru.glas***.entity.User;
 import ru.glas***.repository.UserRepository;
 
@@ -41,5 +42,11 @@ public class WebController {
             return "indexMainPage";
         }
         return "indexSignIn";
+    }
+    @RequestMapping("/test")
+    public String test(Model model)
+    {
+        model.addAttribute("comment",new Comments());
+        return "comments";
     }
 }
