27d3079db9802bb05cd79f398bc7c7cdbb327cf5
Hostile359 <Hostile359@github.com>
2019-08-22 16:57:29 +0700

    video_page test

---
+++ src/main/java/ru/glastube/controller/VideoController.java
@@ -0,0 +1,31 @@
+package ru.glas***.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.glas***.entity.Video;
+import ru.glas***.repository.VideoRepository;
+
+import java.util.Optional;
+
+@Controller
+public class VideoController {
+    @Autowired
+    private VideoRepository crudRep;
+
+    @RequestMapping("/test_video")
+    public String VideoPage(@RequestParam("id") Integer id, Model model) {
+        /*List<User> users = new ArrayList<>();
+        crudRep.findAll().forEach(crud -> {users.add(crud);});
+        users.forEach(User::printInf);*/
+        System.out.println(crudRep.findById(id).get());
+        model.addAttribute("video", crudRep.findById(id).get());
+
+        return "VideoPage";
+    }
+
+
+}
---
+++ src/main/java/ru/glastube/entity/Video.java
@@ -0,0 +1,22 @@
+package ru.glas***.entity;
+
+import lombok.*;
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Entity
+public class Video {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @Getter @Setter private Integer id;
+    @Getter @Setter private String name;
+    @Getter @Setter private String author;
+    @Getter @Setter private String path;
+
+    public Video(String name, String author, String path) {
+        this.name = name;
+        this.author = author;
+        this.path = path;
+    }
+}
