7c9cf8e139973066311db19d4b55e3df4c042952
Stalker2973 <omelete@mail.ru>
2019-08-26 13:18:58 +0700

    Fix commits

--- src/main/resources/static/comments.js
+++ src/main/resources/static/comments.js
@@ -10,18 +10,22 @@ $(document).ready(function() {
     });
 
     function addComment(){
-        //var text = document.getElementById("comment").value;
-        $.get("/add_comment?id_user=123&text=" + document.getElementById("comment").value,function(data){
-            $.get("/all_comments", function(data){
-                var str = "";
-                for(var i = data.length - 1; i >= 0; i--) {
-                    console.log(data[i]);
-                    str += data[i].id_user + "[" + data[i].date + "]:" + data[i].text + "<br>";
-                }
-                $(".comments").html(str);
+        var text = document.getElementById("comment").value;
+        if (text.trim() === ""){
+            alert("Заполните поле для ввода");
+        } else {
+            $.get("/add_comment?id_user=123&text=" + text,function(data){
+                $.get("/all_comments", function(data){
+                    var str = "";
+                    for(var i = data.length - 1; i >= 0; i--) {
+                        console.log(data[i]);
+                        str += data[i].id_user + "[" + data[i].date + "]:" + data[i].text + "<br>";
+                    }
+                    $(".comments").html(str);
+                });
+                console.log(data);
             });
-            console.log(data);
-        });
+        }
         //console.log(text);
         //$.post();
 
