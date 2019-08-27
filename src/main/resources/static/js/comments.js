58f73a8c6a5eb559d327cad742db542575b10ec1
Stalker2973 <omelete@mail.ru>
2019-08-27 11:12:26 +0700

    Clear spaces

--- src/main/resources/static/js/comments.js
+++ src/main/resources/static/js/comments.js
@@ -9,25 +9,25 @@ $(document).ready(function() {
     });
 });
 
-    function addComment(){
-        var text = document.getElementById("comment").value;
-        if (text.trim() === ""){
-            alert("Заполните поле для ввода");
-        } else {
-            $.get("/add_comment?id_user=123&text=" + text,function(data){
-                $.get("/all_comments", function(data){
-                    var str = "";
-                    for(var i = data.length - 1; i >= 0; i--) {
-                        console.log(data[i]);
-                        str += data[i].id_user + "[" + data[i].date + "]:" + data[i].text + "<br>";
-                    }
-                    $(".comments").html(str);
-                    document.getElementById("comment").value = "";
-                });
-                console.log(data);
+function addComment(){
+    var text = document.getElementById("comment").value.trim();
+    if (text.trim() === ""){
+        alert("Заполните поле для ввода");
+    } else {
+        $.get("/add_comment?id_user=123&text=" + text,function(data){
+            $.get("/all_comments", function(data){
+                var str = "";
+                for(var i = data.length - 1; i >= 0; i--) {
+                    console.log(data[i]);
+                    str += data[i].id_user + "[" + data[i].date + "]:" + data[i].text + "<br>";
+                }
+                $(".comments").html(str);
+                document.getElementById("comment").value = "";
             });
-        }
-        //console.log(text);
-        //$.post();
-
+            console.log(data);
+        });
     }
+    //console.log(text);
+    //$.post();
+
+}
