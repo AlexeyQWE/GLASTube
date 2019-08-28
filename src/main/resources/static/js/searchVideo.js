c00cab051fcb24b4274db5ec1134489d810450a2
Stalker2973 <omelete@mail.ru>
2019-08-28 18:31:27 +0700

    Search

--- src/main/resources/static/js/searchVideo.js
+++ src/main/resources/static/js/searchVideo.js
@@ -4,8 +4,17 @@ function searchVideo(){
 //    var text = document.getElementsByClassName("form-inline").value;
     var text = document.getElementById("textSearch").value;
     console.log(text);
-    $.get("/resultSearch?text=" + text, function(data){
-
-
-    });
+    if (text.trim() === ""){
+        alert("Search field must not be empty");
+    } else {
+        $.get("/resultSearch?text=" + text, function(data){
+            var str = "";
+            for(var i = 0; i < data.length; i++) {
+                console.log(data[i]);
+                str += data[i].author + "[" + data[i].id + "]:" + data[i].name + "<br>";
+            }
+            $(".temp").html(str);
+            document.getElementsByClassName("temp").value = "";
+        });
+    }
 }
