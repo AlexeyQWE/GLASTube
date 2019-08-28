96b667dc48dae43a8e82bc381463ba8e5c163e7e
Hostile359 <Hostile359@github.com>
2019-08-28 18:30:44 +0700

    profile's page

--- src/main/java/ru/glastube/repository/VideoRepository.java
+++ src/main/java/ru/glastube/repository/VideoRepository.java
@@ -1,8 +1,12 @@
 package ru.glas***.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import ru.glas***.entity.User;
 import ru.glas***.entity.Video;
+
+import java.util.List;
 
 public interface VideoRepository extends CrudRepository<Video, Integer> {
     Video findByName(String name);
+    List<Video> findAllByAuthor(User author);
 }
