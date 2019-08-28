7c7d391f1ccba0219a69317b2ecbbbdd579f0fbf
Hostile359 <Hostile359@github.com>
2019-08-28 19:08:22 +0700

    search

--- src/main/resources/static/js/searchVideo.js
+++ src/main/resources/static/js/searchVideo.js
@@ -7,14 +7,8 @@ function searchVideo(){
     if (text.trim() === ""){
         alert("Search field must not be empty");
     } else {
-        $.get("/resultSearch?text=" + text, function(data){
-            var str = "";
-            for(var i = 0; i < data.length; i++) {
-                console.log(data[i]);
-                str += data[i].author + "[" + data[i].id + "]:" + data[i].name + "<br>";
-            }
-            $(".temp").html(str);
-            document.getElementsByClassName("temp").value = "";
-        });
+        console.log("kekekekekek");
+        location.href = "/resultSearch?text=" + text;
+//        $.get("/resultSearch?text=" + text);
     }
 }
