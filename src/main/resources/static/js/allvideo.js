d5c7f7be9c79503af3badce80c05abc79bd381a3
Hostile359 <Hostile359@github.com>
2019-08-29 18:19:56 +0700

    fixed videos on main page

--- src/main/resources/static/js/allvideo.js
+++ src/main/resources/static/js/allvideo.js
@@ -8,24 +8,27 @@ $(document).ready(function() {
                         console.log(i);
                         if (i == 3 || i == 0) {
                             str1 += "<div class=\"card-img\">";
-                            str1 += "<video th:src=" + data[i].path + " controls></video>";
+                            str1 += "<video src=" + data[i].path + " controls width=\"400\" height=\"300\"></video>";
                             str1 += "<div class=\"card-footer\"><a href=\"/watch?id=" + (data[i].id) + "\" class=\"card-link\">" + data[i].name + "</a>";
                             str1 += "</div></div>";
                             $("#videoList1").html(str1);
                         } else if (i == 1 || i == 4) {
                             str2 += "<div class=\"card-img\">";
-                            str2 += "<video th:src=" + data[i].path + " controls></video>";
+                            str2 += "<video src=" + data[i].path + " controls width=\"400\" height=\"300\"></video>";
 
                             str2 += "<div class=\"card-footer\"><a href=\"/watch?id=" + (data[i].id) + "\" class=\"card-link\">" + data[i].name + "</a>";
                             str2 += "</div></div>";
                             $("#videoList2").html(str2);
                         } else if (i == 5 || i == 2) {
                             str3 += "<div class=\"card-img\">";
-                            str3 += "<video th:src=" + data[i].path + " controls></video>";
+                            str3 += "<video src=" + data[i].path + " controls width=\"400\" height=\"300\"></video>";
                             str3 += "<div class=\"card-footer\"><a href=\"/watch?id=" + (data[i].id) + "\" class=\"card-link\">" + data[i].name + "</a>";
                             str3 += "</div></div>";
                             $("#videoList3").html(str3);
                         }
+                        console.log(str1);
+                        console.log(str2);
+                        console.log(str3);
                     }
             });
         });
